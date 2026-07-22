d9a51a651a09cdf50d6fe394c82633e8b8cf21ae
dtbuild <dtbuild@rigil>
2026-07-22 14:51:47 +0000

    1eaf8bcd5b7efcfe80b8c101ed4c352b25acf9e6 Release 3.0.0 ready
    Sync to source repo @1eaf8bcd5b7efcfe80b8c101ed4c352b25acf9e6

--- js/scroller.bootstrap4.min.mjs
+++ js/scroller.bootstrap4.min.mjs
@@ -1,4 +1,4 @@
-/*! Scroller Bootstrap 4 styling 3.0.0-beta.1 for DataTables
+/*! Scroller Bootstrap 4 styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs4";import Scroller from"datatables.net-scroller";export default DataTable;
--- js/scroller.bootstrap4.mjs
+++ js/scroller.bootstrap4.mjs
@@ -1,4 +1,4 @@
-/*! Scroller Bootstrap 4 styling 3.0.0-beta.1 for DataTables
+/*! Scroller Bootstrap 4 styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
